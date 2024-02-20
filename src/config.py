a29297ba1ff98b3983b7dea994b270f7bb52a397
ahernot <70817470+ahernot@users.noreply.github.com>
2024-02-20 12:18:22 +0100

    add default variables (dirnames, years)

--- src/config.py
+++ src/config.py
@@ -3,3 +3,10 @@ PATH_CAWAQS = '~/cawaqs'
 DIRPATH_DATA = '/home/anatole/Documents/DATA_CAWAQS/SEINE_3C/DATA_SEINE_SIMPLE'
 DIRPATH_OBS  = '/home/anatole/Documents/DATA_CAWAQS/SEINE_3C/DATA_OBS/TS_OBS_DISCHARGE'
 DIRPATH_OUT  = '/home/anatole/Documents/DATA_CAWAQS_OUT/'
+
+YEAR_START = 2005
+YEAR_STOP = 2023
+
+DIRNAME_PROJ = 'Project'
+DIRNAME_PROJ_ITER = 'RUN_CAL_1'  # TODO: implement iterations incrementation within a project
+DIRNAME_PROJ_POSTPROC = 'POST_PROCESSING'
