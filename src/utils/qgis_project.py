16e8466fb2c7f28fddeaa5eda4587e4c3e8084f9
ahernot <70817470+ahernot@users.noreply.github.com>
2024-02-20 15:44:18 +0100

    create qgis_project

---
+++ src/utils/qgis_project.py
@@ -0,0 +1,4 @@
+from qgis.core import QgsProject
+
+QGS_PROJECT = QgsProject.instance()
+QGS_PROJECT.read('/home/anatole/Documents/DATA_CAWAQS/SEINE_3C/Projet_SIG/Modele_Seine_Simple_DataSelected2024.qgs')
