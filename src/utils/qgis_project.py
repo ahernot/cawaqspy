2e141799e8e907cce8659142853b09cccd9e1687
ahernot <70817470+ahernot@users.noreply.github.com>
2024-02-20 16:05:14 +0100

    update qgis project initialization

--- src/utils/qgis_project.py
+++ src/utils/qgis_project.py
@@ -1,4 +1,15 @@
-from qgis.core import QgsProject
+from qgis.core import QgsProject, QgsApplication
 
+
+# Initialize QGis application
+PATH_QGIS = '/usr/bin/qgis'
+QgsApplication.setPrefixPath(PATH_QGIS, True)
+# QgsApplication.initQgis()
+
+
+# Create QGis project instance
+PATH_PROJECT = '$HOME/anatole/Documents/DATA_CAWAQS/SEINE_3C/Projet_SIG/Modele_Seine_Simple_DataSelected2024.qgs'
 QGS_PROJECT = QgsProject.instance()
-QGS_PROJECT.read('/home/anatole/Documents/DATA_CAWAQS/SEINE_3C/Projet_SIG/Modele_Seine_Simple_DataSelected2024.qgs')
+success = QGS_PROJECT.read(PATH_PROJECT)
+
+print(success, QGS_PROJECT)
