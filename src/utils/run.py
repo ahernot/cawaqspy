fd98f5c27a00ec6e6aacb0c09e4a5cb08d4f4593
ahernot <70817470+ahernot@users.noreply.github.com>
2024-02-20 12:18:59 +0100

    add full pipeline for pbias calculation (untested)

--- src/utils/run.py
+++ src/utils/run.py
@@ -1,32 +1,89 @@
 import os
+import subprocess
 
-from src.utils import build_command_file
+from src.utils.build_command_file import build_command_file
+from src.utils.build_cawaqsviz_config import build_config_geometries
 from src.config import *
+
+from src.cawaqsviz_backend.Config import ConfigGeometry
+from src.cawaqsviz_backend.ExploreData import ExploreData
+from src.cawaqsviz_backend.StatisticalCriteria import StatisticalCriteria
 
 
 
 # Run with a DONSUR path
-def run (verbose = False):
+def run (**kwargs) -> dict:  # TODO: DONSUR path
+    """_summary_
 
-    # Build output directory
-    dirname_out_proj = 'TestProj'
-    dirpath_out_proj = os.path.join(DIRPATH_OUT, dirname_out_proj)
+    Kwargs:
+        dirname_proj (str):
+        dirname_proj_postproc (str):
+        verbose (bool): 
 
+    Returns:
+        dict: _description_
+    """
+
+    # Read kwargs
+    verbose = kwargs.get('verbose', False)
+
+
+
+    # Build project directory
+    dirname_proj = kwargs.get('dirname_proj', DIRNAME_PROJ)
+    dirpath_proj = os.path.join(DIRPATH_OUT, dirname_proj)
     try:
-        os.makedirs(dirpath_out_proj)
+        os.makedirs(dirpath_proj)
     except FileExistsError:
         pass
-    
-
-    build_command_file.DIRPATH_DATA = DIRPATH_DATA
-    build_command_file.DIRPATH_OUTPUT = dirpath_out_proj
-    build_command_file.N_THREADS = 8
-    build_command_file.DONSUR = 'DONSUR_BU28.txt'  # TODO: generate on the fly
-    build_command_file.LIEN_BU_MTO = 'LIEN_BU28_MTO.txt'
-    build_command_file.LIEN_ELE_BU_CPROD = 'LIEN_ELE_BU_CPROD.txt'
 
     # Build command file
     DIRPATH_COMM = os.path.join(DIRPATH_DATA, 'Cmd_files')
-    path_command_file = os.path.join(DIRPATH_COMM, f'{dirname_out_proj}.COMM')
-    build_command_file.build(path_command_file)
-    if verbose: print(f'Built command file in path "{path_command_file}"')
+    path_command_file = os.path.join(DIRPATH_COMM, f'{dirname_proj}.COMM')
+    build_command_file(
+        path_command_file,
+        dirpath_data = DIRPATH_DATA,
+        dirpath_output = dirpath_proj,
+        n_threads = 8,
+        donsur = 'donsur_BU28.txt',
+        lien_bu_mto = 'LIEN_BU28_MTO.txt',
+        verbose = verbose
+    )
+
+    # Run CaWaQS from command line
+    path_log = os.path.join(dirpath_proj, f'{dirname_proj}.log')
+    command = f'{PATH_CAWAQS} {path_command_file} {path_log}'
+    if verbose: print(command)
+    # os.system(command)  # TODO: --quiet
+    subprocess.run(command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.PIPE)
+
+
+
+    # Build post-processing directory inside of the project directory
+    dirname_proj_postproc = kwargs.get('dirname_proj_postproc', DIRNAME_PROJ_POSTPROC)
+    dirpath_proj_postproc = os.path.join(dirpath_proj, dirname_proj_postproc)
+    try:
+        os.makedirs(dirpath_proj_postproc)
+    except FileExistsError:
+        pass
+
+    # Build geometries config file
+    config_geometries_dict = build_config_geometries()
+    config_geometry = ConfigGeometry.fromUnformattedDict(config_geometries_dict)
+
+    # Generate ExploreData instance    
+    exd = ExploreData(
+        ids_compartments=config_geometry.idCompartments,
+        config=config_geometry,
+        out_caw_directory=os.path.join(dirpath_proj, DIRNAME_PROJ_ITER),
+        obs_directory=DIRPATH_OBS,
+        post_process_directory=dirpath_proj_postproc,
+        s_year=YEAR_START,  # TODO: start in 2006 (YEAR_START+1)
+        e_year=YEAR_STOP
+    )
+
+    # Compute pbiases
+    sc = StatisticalCriteria(exd=exd)
+    pbiases_dict = sc.run()
+
+    return pbiases_dict
