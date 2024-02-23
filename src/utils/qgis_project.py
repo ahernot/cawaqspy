568a4c93554172398d2b402fb4116f8a8cb212c8
ahernot <70817470+ahernot@users.noreply.github.com>
2024-02-23 14:14:22 +0100

    fix qgis instance creation

--- src/utils/qgis_project.py
+++ src/utils/qgis_project.py
@@ -1,6 +1,9 @@
+import os
 from qgis.core import QgsProject, QgsApplication
-# from src.config import PATH_QGIS
+# from src.config import PATH_QGIS  # TODO
 PATH_QGIS = '/usr/bin/qgis'
+
+os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = '/usr/lib/aarch64-linux-gnu/qt5/plugins/platforms'
 
 
 # Initialize QGis application
@@ -10,10 +13,12 @@ qgs_application.initQgis()
 
 
 # Create QGis project instance
-# PATH_PROJECT = '$HOME/anatole/Documents/DATA_CAWAQS/SEINE_3C/Projet_SIG/Modele_Seine_Simple_DataSelected2024.qgs'
-QGS_PROJECT = QgsProject.instance()
-# success = QGS_PROJECT.read(PATH_PROJECT)
 
+# PATH_PROJECT = '$HOME/anatole/Documents/DATA_CAWAQS/SEINE_3C/Projet_SIG/Modele_Seine_Simple_DataSelected2024.qgs'
+PATH_PROJECT = '/home/anatole/Documents/DATA_CAWAQS/SEINE_3C/Projet_SIG/Modele_Seine_Simple_DataSelected2024.qgs'
+QGS_PROJECT = QgsProject.instance()
+success = QGS_PROJECT.read(PATH_PROJECT)
+# print(os.path.exists(PATH_PROJECT))
 # print(success, QGS_PROJECT)
 
 
