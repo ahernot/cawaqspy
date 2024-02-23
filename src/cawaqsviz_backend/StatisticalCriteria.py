3dc087dbdd24aadceab6303472f182106e0df5b0
ahernot <70817470+ahernot@users.noreply.github.com>
2024-02-23 15:09:31 +0100

    generalize statistical criteria calculation

--- src/cawaqsviz_backend/StatisticalCriteria.py
+++ src/cawaqsviz_backend/StatisticalCriteria.py
@@ -3,7 +3,7 @@ import numpy as np
 
 from src.cawaqsviz_backend.parameters import *
 from src.cawaqsviz_backend.ExploreData import ExploreData
-from src.cawaqsviz_backend.tools.Criteria import pbias  # , averageSimObsRatio
+from src.cawaqsviz_backend.tools.Criteria import *  # pbias, etc
 
 
 
@@ -15,9 +15,7 @@ class StatisticalCriteria:
         self.exd = exd
 
         # Parameters
-        self.calc_pbias = True
-        self.check_average_simobs = False
-        self.obstype = 'Discharge'  # ['', 'Discharge', 'Hydraulic Head']
+        self.obstype = 'Discharge'  # 'Hydraulic Head'
         self.unit = 'l/s'  # ['l/s', 'm3/s']
 
 
@@ -33,8 +31,8 @@ class StatisticalCriteria:
         id_compartment, outtype, param = self.getSimObsResolutionOutputType()
         compartment = self.exd.compartments[id_compartment]
 
-        # Read Sim and Obs Data, Calc Perf stats and Add CRIT Layer in the current projet
-        pbiases_dict = dict()
+        # Read Sim and Obs Data, calculate Perf stats and Add CRIT Layer in the current projet
+        stats_dict = dict()
         for obs_point in compartment.obs.obs_points:
             
             if verbose: print(f'\nObservation name point {obs_point.name}')
@@ -56,9 +54,9 @@ class StatisticalCriteria:
 
             # Calculate performance (biases, etc.)
             crits = self.calcPerfStats(simobsdf['sim'].values, simobsdf['obs'].values)
-            pbiases_dict[obs_point.name] = crits
+            stats_dict[obs_point.name] = crits
 
-        return pbiases_dict
+        return stats_dict
         
 
     def getSimObsResolutionOutputType (self):
@@ -83,18 +81,22 @@ class StatisticalCriteria:
 
         # Initialize crits dictionary
         crits = dict()
-        crits['AObs'] = float(np.nanmean(obs_data))
-        crits['ASim'] = float(np.nanmean(sim_data))
-        crits['StdObs'] = float(np.nanstd(obs_data))
-        crits['StdSim'] = float(np.nanstd(sim_data))
+        crits['avg_obs'] = float(np.nanmean(obs_data))
+        crits['avg_sim'] = float(np.nanmean(sim_data))
+        crits['std_obs'] = float(np.nanstd(obs_data))
+        crits['std_sim'] = float(np.nanstd(sim_data))
+        crits['n_obs'] = countNObservation(obs_data)
 
-        # Compute pbias
-        if self.calc_pbias:
-            crits['PBIAS'] = float(pbias(sim_data, obs_data))
-
-        # Compute average sim/obs
-        if self.check_average_simobs:
-            raise NotImplementedError('Only pbias is available to calculate')
-            crits['AVERAGE OBS/SIM'] = float(averageSimObsRatio(sim_data, obs_data))
+        # Compute statistical criteria for data
+        crits['stack_couples'] = float(stackCouples(sim_data, obs_data))
+        crits['sim_obs_ratio_avg'] = float(averageSimObsRatio(sim_data, obs_data))
+        crits['bias'] = float(bias(sim_data, obs_data))
+        crits['pbias'] = float(pbias(sim_data, obs_data))
+        crits['pbias_norm_std'] = float(pbiasNormStD(sim_data, obs_data))
+        crits['std_ratio'] = float(standardDeviationRatio(sim_data, obs_data))
+        crits['pearson_correlation'] = float(correlationPearson(sim_data, obs_data))
+        crits['kge'] = float(kge(sim_data, obs_data))
+        crits['nash'] = float(nash(sim_data, obs_data))
+        crits['rmse'] = float(rmse(sim_data, obs_data))
 
         return crits
