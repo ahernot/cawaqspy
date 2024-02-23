4c1e88bc5c5f9f338b43405c36f40d617aa53d51
ahernot <70817470+ahernot@users.noreply.github.com>
2024-02-23 15:15:14 +0100

    create json encoder

---
+++ src/resources/json_encoders.py
@@ -0,0 +1,10 @@
+import numpy as np
+import json
+
+class NumpyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.int64):
+            return int(obj)
+        if isinstance(obj, np.float64):
+            return float(obj)
+        return json.JSONEncoder.default(self, obj)
