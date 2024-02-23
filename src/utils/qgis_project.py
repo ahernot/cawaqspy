71c148926909f140a4de1e0c33845bf58202b9b2
ahernot <70817470+ahernot@users.noreply.github.com>
2024-02-23 14:40:01 +0100

    fixes

--- src/utils/qgis_project.py
+++ src/utils/qgis_project.py
@@ -1,7 +1,6 @@
 import os
 from qgis.core import QgsProject, QgsApplication
-# from src.config import PATH_QGIS  # TODO
-PATH_QGIS = '/usr/bin/qgis'
+from src.config import PATH_QGIS
 
 os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = '/usr/lib/aarch64-linux-gnu/qt5/plugins/platforms'
 
@@ -13,13 +12,11 @@ qgs_application.initQgis()
 
 
 # Create QGis project instance
-
-# PATH_PROJECT = '$HOME/anatole/Documents/DATA_CAWAQS/SEINE_3C/Projet_SIG/Modele_Seine_Simple_DataSelected2024.qgs'
 PATH_PROJECT = '/home/anatole/Documents/DATA_CAWAQS/SEINE_3C/Projet_SIG/Modele_Seine_Simple_DataSelected2024.qgs'
 QGS_PROJECT = QgsProject.instance()
 success = QGS_PROJECT.read(PATH_PROJECT)
-# print(os.path.exists(PATH_PROJECT))
-# print(success, QGS_PROJECT)
 
 
-# qgs_application.exitQgis()  # TODO: at the end of the code
+# TODO: at the end of the code
+def exit():
+    qgs_application.exitQgis()
