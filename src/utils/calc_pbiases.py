5a83d72c565458e8cab9d1ddedb3784eeb447d9c
ahernot <70817470+ahernot@users.noreply.github.com>
2024-02-23 14:40:09 +0100

    cleanup, add run options

--- src/utils/calc_pbiases.py
+++ src/utils/calc_pbiases.py
@@ -1,9 +1,9 @@
 import os
 import subprocess
 
-# from src.cawaqsviz_backend.Config import ConfigGeometry
-# from src.cawaqsviz_backend.ExploreData import ExploreData
-# from src.cawaqsviz_backend.StatisticalCriteria import StatisticalCriteria
+from src.cawaqsviz_backend.Config import ConfigGeometry
+from src.cawaqsviz_backend.ExploreData import ExploreData
+from src.cawaqsviz_backend.StatisticalCriteria import StatisticalCriteria
 
 from src.utils.build_command_file import build_command_file
 from src.utils.build_cawaqsviz_config import build_config_geometries, build_config_project
@@ -20,13 +20,15 @@ def calc_pbiases (**kwargs) -> dict:  # TODO: DONSUR path <= wrap calc_pbiases i
         dirname_proj_postproc (str):
         verbose (bool): 
 
+        run_cawaqs (bool)
+        run_cawaqsviz (bool)
+
     Returns:
         dict: _description_
     """
 
     # Read kwargs
     verbose = kwargs.get('verbose', False)
-
 
 
     # Build main project directory
@@ -37,70 +39,85 @@ def calc_pbiases (**kwargs) -> dict:  # TODO: DONSUR path <= wrap calc_pbiases i
     except FileExistsError:
         pass
 
-    # Build CaWaQS command file
-    DIRPATH_COMM = os.path.join(DIRPATH_DATA, 'Cmd_files')
-    path_command_file = os.path.join(DIRPATH_COMM, f'{dirname_proj}.COMM')
-    build_command_file(
-        path_command_file,
-        dirpath_data = DIRPATH_DATA,
-        dirpath_output = dirpath_proj,
-        n_threads = 8,
-        donsur = 'DONSUR_BU28.txt',
-        lien_bu_mto = 'LIEN_BU28_MTO.txt',
-        verbose = verbose
-    )
+    
+    # CaWaQS
+    run_cawaqs = kwargs.get('run_cawaqs', True)
+    if run_cawaqs:
 
-    # Run CaWaQS from command line
-    path_log = os.path.join(dirpath_proj, f'{dirname_proj}.log')
-    command = f'{PATH_CAWAQS} {path_command_file} {path_log}'
-    if verbose: print(command)
-    # Run command
-    if verbose: os.system(command)
-    else: subprocess.run(command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.PIPE)  # TODO: catch errors
+        # Build CaWaQS command file
+        DIRPATH_COMM = os.path.join(DIRPATH_DATA, 'Cmd_files')
+        path_command_file = os.path.join(DIRPATH_COMM, f'{dirname_proj}.COMM')
+        build_command_file(
+            path_command_file,
+            dirpath_data = DIRPATH_DATA,
+            dirpath_output = dirpath_proj,
+            n_threads = 8,
+            donsur = 'DONSUR_BU28.txt',
+            lien_bu_mto = 'LIEN_BU28_MTO.txt',
+            verbose = verbose
+        )
+
+        # Run CaWaQS from command line
+        path_log = os.path.join(dirpath_proj, f'{dirname_proj}.log')
+        command = f'{PATH_CAWAQS} {path_command_file} {path_log}'
+        if verbose: print(command)
+        # Run command
+        if verbose: os.system(command)
+        else: subprocess.run(command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.PIPE)  # TODO: catch errors
 
 
 
-    # Build CaWaQSViz post-processing directory inside of the project directory
-    dirname_proj_postproc = kwargs.get('dirname_proj_postproc', DIRNAME_PROJ_POSTPROC)
-    dirpath_proj_postproc = os.path.join(dirpath_proj, dirname_proj_postproc)
-    try:
-        os.makedirs(dirpath_proj_postproc)
-    except FileExistsError:
-        pass
+    # CaWaQSViz
+    run_cawaqsviz = kwargs.get('run_cawaqsviz', True)
+    if run_cawaqsviz:
+        # Build CaWaQSViz post-processing directory inside of the project directory
+        dirname_proj_postproc = kwargs.get('dirname_proj_postproc', DIRNAME_PROJ_POSTPROC)
+        dirpath_proj_postproc = os.path.join(dirpath_proj, dirname_proj_postproc)
+        try:
+            os.makedirs(dirpath_proj_postproc)
+        except FileExistsError:
+            pass
 
-    # Build CaWaQSViz geometries config file
-    config_geometries_dict, path_config_geometries = build_config_geometries(
-        save=True,
-        dirpath_proj=dirpath_proj,
-        verbose=True
-    )
-    # config_geometry = ConfigGeometry.fromUnformattedDict(config_geometries_dict)
+        # Build CaWaQSViz temp directory
+        dirpath_proj_postproc_temp = os.path.join(dirpath_proj_postproc, 'TEMP')
+        try: os.makedirs(dirpath_proj_postproc_temp)
+        except FileExistsError: pass
 
-    # Build CaWaQSViz project config file
-    config_project_dict, path_config_project = build_config_project(
-        dirpath_proj=dirpath_proj,
-        path_config_geometries=path_config_geometries,
-        year_start=2005,
-        year_stop=2023,
-        dirpath_obs=DIRPATH_OBS,
-        dirpath_proj_postproc=dirpath_proj_postproc,
-        save=True,
-        verbose=True
-    )
+        # Build CaWaQSViz geometries config file
+        config_geometries_dict, path_config_geometries = build_config_geometries(
+            save = True,
+            dirpath_proj = dirpath_proj,
+            verbose = verbose
+        )
+        config_geometry = ConfigGeometry.fromUnformattedDict(config_geometries_dict)
 
-    # # Generate CaWaQSViz ExploreData instance    
-    # exd = ExploreData(
-    #     ids_compartments=config_geometry.idCompartments,
-    #     config=config_geometry,
-    #     out_caw_directory=os.path.join(dirpath_proj, DIRNAME_PROJ_ITER),
-    #     obs_directory=DIRPATH_OBS,
-    #     post_process_directory=dirpath_proj_postproc,
-    #     s_year=YEAR_START+1,  # TODO: start in YEAR_START+1
-    #     e_year=YEAR_STOP
-    # )
+        # Build CaWaQSViz project config file
+        config_project_dict, path_config_project = build_config_project(
+            dirpath_proj = dirpath_proj,
+            path_config_geometries = path_config_geometries,
+            year_start = 2005,
+            year_stop = 2023,
+            dirpath_obs = DIRPATH_OBS,
+            dirpath_proj_postproc = dirpath_proj_postproc,
+            save = True,
+            verbose = verbose
+        )
 
-    # # Compute pbiases using CaWaQSViz
-    # sc = StatisticalCriteria(exd=exd)
-    # pbiases_dict = sc.run()
+        # Generate CaWaQSViz ExploreData instance    
+        exd = ExploreData(
+            ids_compartments = config_geometry.idCompartments,
+            config = config_geometry,
+            out_caw_directory = os.path.join(dirpath_proj, DIRNAME_PROJ_ITER),
+            obs_directory = DIRPATH_OBS,
+            post_process_directory = dirpath_proj_postproc,
+            s_year = YEAR_START + 1,  # TODO: start in YEAR_START+1
+            e_year = YEAR_STOP,
+            dirpath_temp = dirpath_proj_postproc_temp
+        )
 
-    # return pbiases_dict
+        # Compute pbiases using CaWaQSViz
+        sc = StatisticalCriteria(exd=exd)
+        pbiases_dict = sc.run()
+
+        return pbiases_dict
+    return None
