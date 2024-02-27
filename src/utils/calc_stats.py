c382c69225f6a7d7f3134e193ad060e4c93365a4
ahernot <70817470+ahernot@users.noreply.github.com>
2024-02-27 14:49:43 +0100

    add temp directory purging

--- src/utils/calc_stats.py
+++ src/utils/calc_stats.py
@@ -87,8 +87,7 @@ def calc_stats (proj_name: str, obstype: str, **kwargs) -> dict:  # TODO: DONSUR
 
         # Build CaWaQSViz temp directory
         dirpath_proj_postproc_temp = os.path.join(dirpath_proj_postproc, 'TEMP')
-        try: os.makedirs(dirpath_proj_postproc_temp)
-        except FileExistsError: pass
+        os.makedirs(dirpath_proj_postproc_temp, exist_ok=True)  # Overwrite if needed
 
         # Build CaWaQSViz geometries config file
         config_geometries_dict, path_config_geometries = build_config_geometries(
