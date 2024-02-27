d9678482463d1192b52a11fa0ffb06029ff9a3db
ahernot <70817470+ahernot@users.noreply.github.com>
2024-02-27 14:38:22 +0100

    build command file

--- src/utils/build_command_file.py
+++ src/utils/build_command_file.py
@@ -1,7 +1,10 @@
 from src.config import *
 
+
+
 # Default variables
 PARAMS_DICT = {
+    'project_name': 'SEINE_SIMPLE',
     'dirpath_data': '/home/anatole/Documents/DATA_CAWAQS/SEINE_3C/DATA_SEINE_SIMPLE',
     'dirpath_output': '/home/anatole/Documents/DATA_CAWAQS_OUT/TestProj',
     'n_threads': N_THREADS,
@@ -10,6 +13,7 @@ PARAMS_DICT = {
     'year_start': YEAR_START,
     'year_stop': YEAR_STOP
 }
+
 
 
 COMMFILE_PATHS = """\
@@ -28,7 +32,7 @@ Output_folder   = {dirpath_output}
 COMMFILE_SETTINGS = """\
 simulation = {{ 
 
-    SEINE_SIMPLE
+    {project_name}
 
     TIME = {{ 
         year_init = {year_start}
@@ -50,70 +54,74 @@ simulation = {{
 }}
 """
 
-COMMFILE_HYDRO_AQUIFER = """\
-AQUIFER = {{
 
-       SETTINGS = {{ 
-           anisotropy_factor = 0.1                 # facteur d'anisotropie verticale     
-           # specific_yield_factor = 0.01           # facteur de conversion S d'UNCONFINED vers CONFINED
-           log_fluxes = NO                         # Bilan de flux à l'échelle de la couche
-           print_lithos = NO                       # Map des lithos affleurantes (si en mode UNCONFINED)
-       }} 
+class CommfileHydro:
+    root = 'HYDRO'
 
-       OMP = {{ NO nthreads = {n_threads} }}
+    AQUIFER = """\
+    AQUIFER = {{
 
-       GW_MESH = {{
-           layer = {{ tertiaire   include grid_TERT.txt }}     
-           layer = {{ craie       include grid_CRAI.txt }}     
-           layer = {{ jurassique  include grid_JURA.txt }}     
-       }}
+        SETTINGS = {{ 
+            anisotropy_factor = 0.1                 # facteur d'anisotropie verticale     
+            # specific_yield_factor = 0.01           # facteur de conversion S d'UNCONFINED vers CONFINED
+            log_fluxes = NO                         # Bilan de flux à l'échelle de la couche
+            print_lithos = NO                       # Map des lithos affleurantes (si en mode UNCONFINED)
+        }} 
 
-       SET_UP = {{
- 
-           setup_layer = {{                   
-               tertiaire
-               param = {{
-                   transm_homo     = {{ uniform 1.12E-2 [m2/s] }}           
-                   storage 	       = {{ include Emmag_TERT.txt }}           
-                   h_ini 		   = {{ include H_ini_TERT.txt }}        
-                   thick 		   = {{ include Thickness_TERT.txt }}       
-                   conductance     = {{ include Cond_drain_TERT.txt }}      
-                   Mean 		   = HARMONIC
-               }}
-               boundary 		   = {{ include Bound_TERT.txt }}           
-           }}
- 
-           setup_layer = {{
-               craie
-               param = {{
-                   transm_homo     = {{ uniform 3.52E-2 [m2/s] }}            
-                   storage 	       = {{ include Emmag_CRAI.txt }}            
-                   h_ini 		   = {{ include H_ini_CRAI.txt }}         
-                   thick 		   = {{ include Thickness_CRAI.txt }}               
-                   conductance     = {{ include Cond_drain_CRAI.txt }}       
-                   Mean 		   = HARMONIC
-               }}
-               boundary 		   = {{ include Bound_CRAI.txt }}            
-           }}
- 
-           setup_layer = {{
-               jurassique
-               param = {{
-                   transm_homo     = {{ uniform 2.20E-2 [m2/s] }}            
-                   storage 	       = {{ include Emmag_JURA.txt }}            
-                   h_ini 		   = {{ include H_ini_JURA.txt }}         
-                   thick 		   = {{ include Thickness_JURA.txt }}        
-                   conductance     = {{ include Cond_drain_JURA.txt }}       
-                   Mean 		   = HARMONIC
-               }}
-               boundary 		   = {{ include Bound_JURA.txt }}                           
-           }}
-       }} 
-   }}
+        OMP = {{ NO nthreads = {n_threads} }}
+
+        GW_MESH = {{
+            layer = {{ tertiaire   include grid_TERT.txt }}     
+            layer = {{ craie       include grid_CRAI.txt }}     
+            layer = {{ jurassique  include grid_JURA.txt }}     
+        }}
+
+        SET_UP = {{
+    
+            setup_layer = {{                   
+                tertiaire
+                param = {{
+                    transm_homo    = {{ uniform 1.12E-2 [m2/s] }}           
+                    storage 	   = {{ include Emmag_TERT.txt }}           
+                    h_ini 		   = {{ include H_ini_TERT.txt }}        
+                    thick 		   = {{ include Thickness_TERT.txt }}       
+                    conductance    = {{ include Cond_drain_TERT.txt }}      
+                    Mean 		   = HARMONIC
+                }}
+                boundary 		   = {{ include Bound_TERT.txt }}           
+            }}
+    
+            setup_layer = {{
+                craie
+                param = {{
+                    transm_homo    = {{ uniform 3.52E-2 [m2/s] }}            
+                    storage 	   = {{ include Emmag_CRAI.txt }}            
+                    h_ini 		   = {{ include H_ini_CRAI.txt }}         
+                    thick 		   = {{ include Thickness_CRAI.txt }}               
+                    conductance    = {{ include Cond_drain_CRAI.txt }}       
+                    Mean 		   = HARMONIC
+                }}
+                boundary 		   = {{ include Bound_CRAI.txt }}            
+            }}
+    
+            setup_layer = {{
+                jurassique
+                param = {{
+                    transm_homo    = {{ uniform 2.20E-2 [m2/s] }}            
+                    storage 	   = {{ include Emmag_JURA.txt }}            
+                    h_ini 		   = {{ include H_ini_JURA.txt }}         
+                    thick 		   = {{ include Thickness_JURA.txt }}        
+                    conductance    = {{ include Cond_drain_JURA.txt }}       
+                    Mean 		   = HARMONIC
+                }}
+                boundary 		   = {{ include Bound_JURA.txt }}                           
+            }}
+        }} 
+    }}
 """
 
-COMMFILE_HYDRO_SURFACE = """\
-SURFACE = {{
+    SURFACE = """\
+    SURFACE = {{
 
         MTO = {{
             mto_path     = {dirpath_data}/METEO
@@ -165,7 +173,120 @@ SURFACE = {{
     }}
 """
 
-# HYDRO -> (AQUIFER, SURFACE, NONSAT)
+    NONSAT = """\
+    NONSAT = {{
+    
+        OMP = 	{{ NO NTHREADS = {n_threads} }}
+
+        param      = {{ include PARAM_NSAT.txt }} 
+        nsat_units = {{ include LIEN_NSAT_PARAM.txt }}        # Zonage uniforme à 0 res. (= Nsat inactif.)
+        nsat_prod  = {{ include LIEN_NSAT_ELE_BU.txt }}
+        nsat_aq    = {{ include LIEN_NSAT_AQF.txt }}           
+    }}
+"""
+
+
+class CommfileOutputs:
+    root = 'OUTPUTS'
+
+    MB_AQ = """\
+    MB_AQ = {{
+    Output_settings = {{ NO
+                        format = UNFORMATTED
+                        print_final_state = YES
+                        time = {{ dt = [d] 1 }}
+                    }}
+    }}
+"""
+
+    H_AQ = """\
+    H_AQ = {{
+    Output_settings = {{ NO
+                        format = UNFORMATTED
+                        print_final_state = YES
+                        time = {{ dt = [d] 1 }}
+                }}
+    }}
+"""
+
+    Q_HYD = """\
+    Q_HYD = {{
+    Output_settings = {{ YES
+                        format = UNFORMATTED
+                        print_final_state = NO
+                        time = {{ dt = [d] 1 }}
+                    }}
+    }}
+"""
+
+    NSAT = """\
+    NSAT = {{
+    Output_settings = {{ NO
+                        format = UNFORMATTED
+                        time = {{ dt = [d] 1 }}
+                    }}
+    }}
+"""
+
+    WATBAL = """\
+    WATBAL = {{
+    Output_settings = {{ YES
+                         format = UNFORMATTED
+                         time = {{ dt = [d] 1 }}
+                         spatial_scale = WATBAL_ELEMENT
+                      }}                   
+    }}
+"""
+
+
+
+
+def build_command_file (path: str, **kwargs):
+
+    # Read kwargs
+    verbose = kwargs.get('verbose', False)
+    include_hydro_aquifer = kwargs.get('include_hydro_aquifer', False)
+
+    # Build hydro block
+    contents_hydro = list()
+    if include_hydro_aquifer: contents_hydro .append(CommfileHydro.AQUIFER)
+    contents_hydro.append(CommfileHydro.SURFACE)
+    contents_hydro.append(CommfileHydro.NONSAT)    
+    block_hydro = 'HYDRO = {{\n\n' + '\n'.join(contents_hydro) + '\n}}  # End of block HYDRO\n'
+
+    # Build outputs block
+    contents_outputs = list()
+    if include_hydro_aquifer: contents_outputs.append(CommfileOutputs.MB_AQ)
+    if include_hydro_aquifer: contents_outputs.append(CommfileOutputs.H_AQ)
+    contents_outputs.append(CommfileOutputs.Q_HYD)
+    contents_outputs.append(CommfileOutputs.NSAT)
+    contents_outputs.append(CommfileOutputs.WATBAL)
+    block_outputs = 'OUTPUTS = {{\n\n' + '\n'.join(contents_outputs) + '\n}}  # End of block OUTPUTS\n'
+
+    # Concatenate blocks
+    commfile_blocks = [
+        COMMFILE_PATHS,
+        COMMFILE_SETTINGS,
+        block_hydro,
+        block_outputs
+    ]
+    commfile_full = '\n'.join(commfile_blocks)
+
+    # Format command file
+    params_dict = PARAMS_DICT.copy()
+    params_dict.update(kwargs)
+    commfile_full_formatted = commfile_full.format(**params_dict)
+
+    # Write to file
+    with open(path, 'w', encoding='utf-8') as file:
+        file.write(commfile_full_formatted)
+
+    # Print success message
+    if verbose:
+        print(f'Built command file in path "{path}"')
+
+
+
 
 
 # Command file template (contains some hard-coded values)
@@ -373,139 +494,3 @@ OUTPUTS = {{
 
 }} # Fin du bloc OUTPUTS
 """
-
-
-COMMFILE_OUTPUTS = """\
-OUTPUTS = {{
-
-#    MB_AQ = {{
-#    Output_settings = {{ NO
-#                        format = UNFORMATTED
-#                        print_final_state = YES
-#                        time = {{ dt = [d] 1 }}
-#                      }}
-#    }}
-
-#    H_AQ = {{
-#    Output_settings = {{ NO
-#                        format = UNFORMATTED
-#                        print_final_state = YES
-#                        time = {{ dt = [d] 1 }}
-#                      }}
-#    }}
-
-    Q_HYD = {{
-    Output_settings = {{ YES
-                        format = UNFORMATTED
-                        print_final_state = NO
-                        time = {{ dt = [d] 1 }}
-                      }}
-    }}
-
-
-#    NSAT = {{
-#    Output_settings = {{ NO
-#                        format = UNFORMATTED
-#                        time = {{ dt = [d] 1 }}
-#                      }}
-#    }}
-
-    WATBAL = {{
-    Output_settings = {{ YES
-                        format = UNFORMATTED
-                        time = {{ dt = [d] 1 }}
-                        spatial_scale = WATBAL_ELEMENT
-                     }}                   
-    }}
-
-}} # Fin du bloc OUTPUTS
-"""
-
-class CommfileOutputs:
-
-    ROOT = """\
-    OUTPUTS = {{
-        {outputs}
-    }}\
-    """
-
-    COMMFILE_OUTPUTS_MB_AQ = """\
-        MB_AQ = {{
-        Output_settings = {{ NO
-                            format = UNFORMATTED
-                            print_final_state = YES
-                            time = {{ dt = [d] 1 }}
-                        }}
-        }}\
-    """
-
-    COMMFILE_OUTPUTS_H_AQ = """\
-        H_AQ = {{
-        Output_settings = {{ NO
-                            format = UNFORMATTED
-                            print_final_state = YES
-                            time = {{ dt = [d] 1 }}
-                    }}
-        }}\
-    """
-
-    COMMFILE_OUTPUTS_Q_HYD = """\
-        Q_HYD = {{
-        Output_settings = {{ YES
-                            format = UNFORMATTED
-                            print_final_state = NO
-                            time = {{ dt = [d] 1 }}
-                        }}
-        }}\
-    """
-
-    COMMFILE_OUTPUTS_NSAT = """\
-        NSAT = {{
-        Output_settings = {{ NO
-                            format = UNFORMATTED
-                            time = {{ dt = [d] 1 }}
-                        }}
-        }}\
-    """
-
-    COMMFILE_OUTPUTS_WATBAL = """\
-    WATBAL = {{
-    Output_settings = {{ YES
-                         format = UNFORMATTED
-                         time = {{ dt = [d] 1 }}
-                         spatial_scale = WATBAL_ELEMENT
-                      }}                   
-    }}\
-"""
-
-
-commfile_outputs_dict = {
-    'ROOT':   CommfileOutputs.ROOT,
-    'MB_AQ':  CommfileOutputs.COMMFILE_OUTPUTS_MB_AQ,
-    'H_AQ':   CommfileOutputs.COMMFILE_OUTPUTS_H_AQ,
-    'Q_HYD':  CommfileOutputs.COMMFILE_OUTPUTS_Q_HYD,
-    'NSAT':   CommfileOutputs.COMMFILE_OUTPUTS_NSAT,
-    'WATBAL': CommfileOutputs.COMMFILE_OUTPUTS_WATBAL
-
-}
-
-
-
-def build_command_file (path: str, **kwargs):
-
-    # Generate command file from template
-    params_dict = PARAMS_DICT.copy()
-    params_dict.update(kwargs)
-    commfile = COMMFILE.format(**params_dict)
-    
-    # Write to file
-    with open(path, 'w', encoding='utf-8') as file:
-        file.write(commfile)
-
-    # Print success message
-    verbose = kwargs.get('verbose', False)
-    if verbose:
-        print(f'Built command file in path "{path}"')
-
-
-
