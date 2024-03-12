8e44152c6cc633c42e5704262c778fae45ac1fbd
ahernot <70817470+ahernot@users.noreply.github.com>
2024-03-12 16:58:16 +0100

    change from print to string output

--- src/utils/command_file.py
+++ src/utils/command_file.py
@@ -24,7 +24,26 @@ def print_command_dict (command_dict: dict, indent_break_max = 0):
         indent_break_max (int): Maximum indentation level at which to print line breaks between elements (0 prints no breaks).
     """
 
-    def __print_recur (subdict: dict, indent = 0, key_len_max = 0, indent_break_max = 0):
+    def __print_recur (
+            subdict: dict,
+            out_list: list = [],
+            indent: int = 0,
+            key_len_max: int = 0,
+            indent_break_max: int = 0
+        ) -> list:
+        """
+        _summary_
+
+        Args:
+            subdict (dict): _description_
+            out_list (list, optional): Current output list. Defaults to [].
+            indent (int, optional): Current indent level. Defaults to 0.
+            key_len_max (int, optional): Max key length for current subdict (for text formatting). Defaults to 0.
+            indent_break_max (int, optional): Maximum indentation level at which to print line breaks between elements (0 prints no breaks). Defaults to 0.
+
+        Returns:
+            list: _description_
+        """
         
         __types_single = (str, int, float, bool, type(None))
         __ichar = '\t'
@@ -48,7 +67,7 @@ def print_command_dict (command_dict: dict, indent_break_max = 0):
                     print_prefix = f'{__serialize(key)} '
                     print_suffix = f'= {{ {__serialize(list(val.keys())[0])} }}'
                     if key_len_max > len(print_prefix): print_prefix += ' ' * (key_len_max - len(print_prefix))
-                    print(__ichar*indent + print_prefix + print_suffix)  # Print
+                    out_list.append (__ichar*indent + print_prefix + print_suffix)  # Print
                 
                 # Multiple subitems: expanded dict writing (recursive call) - no formatting adjustment with spaces
                 else:
@@ -67,9 +86,9 @@ def print_command_dict (command_dict: dict, indent_break_max = 0):
                                 keys_eligible_.append(key_)
                     key_len_max_ = max([len(key) for key in keys_eligible_]) + 1 if keys_eligible_ else 0
 
-                    print(__ichar*indent + f'{__serialize(key)} = {{')
-                    __print_recur(val, indent=indent+__iincr, key_len_max=key_len_max_)
-                    print(__ichar*indent + '}')
+                    out_list.append (__ichar*indent + f'{__serialize(key)} = {{')
+                    out_list = __print_recur(val, out_list=out_list, indent=indent+__iincr, key_len_max=key_len_max_)
+                    out_list.append (__ichar*indent + '}')
 
 
             # Unpack lists to assign the key to each subdictionary
@@ -77,34 +96,37 @@ def print_command_dict (command_dict: dict, indent_break_max = 0):
                 # List contains only non-iterable elements (only print key once)
                 if all((type(v) in __types_single  for v in val)):
                     print_prefix = f'{__serialize(key)} '
-                    print(__ichar*indent + print_prefix + f'= {val[0]}')
+                    out_list.append (__ichar*indent + print_prefix + f'= {val[0]}')
                     for item in val[1:]:
-                        print(__ichar*indent + ' '*len(print_prefix) + f'= {val[0]}')
+                        out_list.append (__ichar*indent + ' '*len(print_prefix) + f'= {val[0]}')
 
                 # List contains dictionary elements
                 else:
                     for item in val:
-                        __print_recur({key: item}, indent=indent)  # Distribute print_recur across all subdicts contained in the list
+                        out_list = __print_recur({key: item}, out_list=out_list, indent=indent)  # Distribute print_recur across all subdicts contained in the list
 
 
             # End node (leaf)
             else:
                 # Inline text (prints value only, formatted as "value") for elements of dict with '__inline__' key
                 if key == '__inline__':
-                    print(__ichar*indent + __serialize(val))
+                    out_list.append (__ichar*indent + __serialize(val))
 
                 # Regular key-pair formatted as "key = value"
                 else:
                     print_prefix = f'{__serialize(key)} '
                     print_suffix = f'= {__serialize(val)}'
                     if key_len_max > len(print_prefix): print_prefix += ' ' * (key_len_max - len(print_prefix))
-                    print(__ichar*indent + print_prefix + print_suffix )
+                    out_list.append (__ichar*indent + print_prefix + print_suffix )
             
-            if indent < indent_break_max:
-                print()
-    
-    return __print_recur(command_dict, indent=0, key_len_max=0, indent_break_max=indent_break_max)
 
+            # Line break between main sections
+            if indent < indent_break_max:
+                out_list.append ('')
+
+        return out_list
+    
+    return '\n'.join( __print_recur(command_dict, out_list=[], indent=0, key_len_max=0, indent_break_max=indent_break_max) )
 
 
 
