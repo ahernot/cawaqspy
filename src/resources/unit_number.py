b8994db228a679f42a575b9d28d5d91a13b583d5
ahernot <70817470+ahernot@users.noreply.github.com>
2024-03-13 12:22:20 +0100

    init

---
+++ src/resources/unit_number.py
@@ -0,0 +1,7 @@
+# from typing import Union
+# class Unit:
+#     def __init__ (self, val: Union[int, float], unit: str = None):
+#         self
+#     def __repr__ (self):
+#         return self.val
+# TODO: number with unit
